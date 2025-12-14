a194fee558a32fd91cde2217664f95134286386e
AlejandroFontesAlbeza <alexfontesalbeza@gmail.com>
2025-12-14 16:16:22 +0100

    initial commit

---
+++ inference.py
@@ -0,0 +1,41 @@
+from train import X_val,Y_val,MNISTModel
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+dataset = pd.read_csv('dataset/test.csv')
+data = np.array(dataset)
+
+
+idx = 102
+image = data[idx]        # shape = (784,)
+image_tensor = torch.tensor(image, dtype=torch.float32).unsqueeze(0)  # añadir batch dim → (1, 784)
+image_tensor /= 255.0
+
+model = MNISTModel()
+
+model.load_state_dict(torch.load("model.pth"))
+model.eval()
+
+def main():
+    img = image_tensor
+
+    img_plt = img.reshape(28,28) * 255.0
+
+    # Inferencia
+    with torch.no_grad():
+        outputs = model(img)
+        predicted_label = torch.argmax(outputs, dim=1).item()
+
+    # Mostrar la imagen
+    plt.gray()
+    plt.imshow(img_plt)
+    plt.title(f"Predicted: {predicted_label}")
+    plt.savefig("resources/label_predicted")
+    print('label predicted')
+
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('dataset/train.csv')
+data = np.array(data)
+m,n = data.shape
+
+
+dataset_val = data[0:1000]
+Y_val = dataset_val[:,0]
+X_val = np.float32(dataset_val[:, 1:n])
+X_val /= 255.0
+
+
+dataset_train = data[1000:m]
+Y_train = dataset_train[:,0]
+X_train = np.float32(dataset_train[:, 1:n])
+X_train /= 255.0
+
+
+Y_val = torch.tensor(Y_val, dtype=torch.long)
+X_val = torch.tensor(X_val, dtype=torch.float32)
+Y_train = torch.tensor(Y_train, dtype=torch.long)
+X_train = torch.tensor(X_train, dtype=torch.float32)
+
+
+Val_dataset = TensorDataset(X_val,Y_val)
+Train_dataset = TensorDataset(X_train,Y_train)
+val_data_loader = DataLoader(Val_dataset, batch_size=10, shuffle=False)
+train_data_loader = DataLoader(Train_dataset, batch_size=10, shuffle=True)
+
+
+
+class MNISTModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(28*28,10),
+            nn.ReLU(),
+            nn.Linear(10,10),
+        )
+
+    def forward(self,x):
+        outputs = self.model(x)
+
+        return outputs
+    
+
+model = MNISTModel()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+
+
+epochs = 50
+
+
+def main():
+
+    accuracies = []
+    losses = []
+    epoch_list = []
+        
+    for epoch in range(epochs):
+
+        running_loss = 0
+        total = 0
+        correct = 0
+
+        model.train()
+        for Xbatch,Ybatch in train_data_loader:
+            optimizer.zero_grad()
+            outputs = model(Xbatch)
+            loss = criterion(outputs,Ybatch)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()/Xbatch.size(0)
+
+        epoch_training_loss = running_loss / len(train_data_loader)
+
+        model.eval()
+
+        with torch.no_grad():
+            for Xbatch,Ybatch in train_data_loader:
+                outputs = model(Xbatch)
+                _,predictions = torch.max(outputs,1)
+                total += Ybatch.size(0)
+                correct += (predictions == Ybatch).sum().item()
+
+        accuracy = correct/total
+
+        print(f'Epoch {epoch}, accuracy {accuracy}, T_loss {epoch_training_loss}')
+
+        accuracies.append(accuracy)
+        losses.append(epoch_training_loss)
+        epoch_list.append(epoch)
+
+    
+    fig,ax = plt.subplots(1,2,figsize = (10,4))
+
+    ax[0].plot(epoch_list, accuracies)
+    ax[0].set_title('Accuracy')
+    ax[0].set_xlabel('Epoch')
+    ax[0].set_ylabel('Accuracy')
+
+    ax[1].plot(epoch_list, losses)
+    ax[1].set_title('Training Loss')
+    ax[1].set_xlabel('Epoch')
+    ax[1].set_ylabel('Loss')
+
+    plt.savefig('resources/training_fig.png')
+
+
+
+    torch.save(model.state_dict(), 'model.pth')
+    print('modelo guardado')
+
+
+
+if __name__ == "__main__":
+
+    main()
+        
+
